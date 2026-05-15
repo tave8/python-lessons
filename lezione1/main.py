17c7c4bc6d0012f2f1bef03085f810472026d783
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-05-15 18:23:03 +0200

    prima lezione

---
+++ lezione1/main.py
@@ -0,0 +1,53 @@
+
+# una variabile è una scatola
+
+# x = 1 non si legge "x è uguale a 1?"
+# ma si legge "assegno il valore a destra alla scatola che ha nome con il nome definito a sinistra"
+
+# a sinistra c'è il nome della scatola, a destra c'è il valore della scatola
+
+# tipo primitivo = una cella di memoria, non posso spezzetterlo ulteriormente
+
+# numeri, stringhe, booleani, none
+
+x = 1
+z = True
+t = None
+y = "ciao"
+
+# tipo complesso
+
+# python tratta una stringa come se fosse "iterabile" al pari di una lista
+lista: str = "bicicletta"
+lista = []
+
+# indice 0 = primo elemento
+# indice 1 = secondo elemento
+
+# perché da errore: dammi il primo elemento di questa lista
+# vado nella lista e non ci sono elementi, che ti do? -> errore di indice
+print(lista[0])
+
+
+# print(lista)
+
+
+
+matrice = [
+    [1, 2, 3],
+    [4, 5, 6],
+]
+
+essereUmano = {
+    "numeroGambe": 2,
+    "numeroTeste": 1
+}
+
+
+
+
+
+
+
+
+
